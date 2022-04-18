9f144fa3320ebc5f646360472614eb52579a52a3
Peyton Shetler <peyton.shetler@gmail.com>
2022-04-18 11:30:51 -0500

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Peyton Shetler on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
